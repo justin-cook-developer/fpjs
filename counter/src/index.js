99b723cc571aa4cca36e4b75fbb0298585bdf5b0
Justin Cook <justin.cook.dev@gmail.com>
2019-02-28 00:26:23 -0800

    Created the view function.

---
+++ counter/src/index.js
@@ -0,0 +1,21 @@
+import h from 'hyperscript'
+import hh from 'hyperscript-helpers'
+
+const { div, button } = hh(h);
+
+const initModel = 0;
+
+function view(model) {
+  return div([
+    div({className: 'mv2' }, `Count: ${model}`),
+    button({ className: 'pv1 ph2 mr2',
+      onclick: () => console.log('clicked jjj')
+    }, '+'),
+    button({ className: 'pv1 ph2',
+      onclick: () => console.log('clicked ddddd')
+    }, '-'),
+  ]);
+}
+
+const rootNode = document.getElementById('app');
+rootNode.appendChild(view(initModel));
