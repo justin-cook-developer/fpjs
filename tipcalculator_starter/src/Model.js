1e4db34efdfe31aa867689f4dee6a4b384a3f613
Justin Cook <justin.cook.dev@gmail.com>
2019-03-04 14:17:18 -0800

    Finished the tip calculator

--- tipcalculator_starter/src/Model.js
+++ tipcalculator_starter/src/Model.js
@@ -1,5 +1,8 @@
 const initModel = {
-  
-};
+  bill:'0.09',
+  tipPercentage: '1.11',
+  tipAmount: 0.01,
+  total: 0.09,
+}
 
 export default initModel;
--- tipcalculator_starter/src/View.js
+++ tipcalculator_starter/src/View.js
@@ -2,17 +2,58 @@ import * as R from 'ramda';
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 
+import { billChangeCreator, percentChangeCreator } from './Update';
+
 const {
   div,
   h1,
   pre,
-} = hh(h);
+  input,
+  p
+} = hh(h)
 
+
+function inputItem(dispatch, title, value, handleInput) {
+  return div([
+    p(title),
+    input({
+      type: 'text',
+      value: value,
+      oninput: e => {;
+        dispatch(handleInput(e.target.value));
+      },
+    }),
+  ]);
+}
+
+function displayItem(title, value) {
+  const stringValue = value.toString();
+  return div({ className: 'flex' }, [
+    p(title),
+    p(stringValue),
+  ]);
+}
+
+function inputGroup(dispatch, model) {
+  return div([
+    inputItem(dispatch, 'Bill Amount', model.bill, billChangeCreator),
+    inputItem(dispatch, 'Tip %', model.tipPercentage, percentChangeCreator),
+  ]);
+}
+
+function displayGroup(model) {
+  return div([
+    displayItem('Tip:', model.tipAmount),
+    displayItem('Total Amount:', model.total),
+  ]);
+}
 
 function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Tip Calculator'),
-    pre(JSON.stringify(model, null, 2)),
+    // pre(JSON.stringify(model, null, 2)),
+    inputGroup(dispatch, model),
+    displayGroup(model),
   ]);
 }
 
