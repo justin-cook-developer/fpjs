3c17dcbc36b1dfaec7504ec2ea376dfd43bcf431
Justin Cook <justin.cook.dev@gmail.com>
2019-03-05 17:23:19 -0800

    Initial model and question view.

--- flashcard_starter/src/Model.js
+++ flashcard_starter/src/Model.js
@@ -1,3 +1,14 @@
-const initModel = {};
+const initModel = {
+  nextId: 1,
+  cards: [
+    {
+      question : 'What is a pure function?',
+      answer : 'A pure function must always return the same output given the same input, and its execution must not depend on the outside state. It must not cause any side effects.',
+      displayMode: 'question',
+      rank: 0,
+      id: 0,
+    },
+  ],
+};
 
 export default initModel;
--- flashcard_starter/src/View.js
+++ flashcard_starter/src/View.js
@@ -2,11 +2,49 @@ import * as R from 'ramda';
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 
-const { div, h1 } = hh(h);
+const { div, h1, label, input, h2, p, pre, a, button } = hh(h);
+
+const showQuestionComponent = (dispatch, model) => {
+  return div([
+    h2('Question'),
+    p(model.question)
+  ]);
+}
+
+const deleteComponent = dispatch => button('X');
+
+const answerLinkComponent = dispatch => a('Show me the answer');
+
+const questionMode = (dispatch, card) => {
+  return div([
+    deleteComponent(dispatch),
+    showQuestionComponent(dispatch, card),
+    answerLinkComponent(dispatch)
+  ]);
+}
+
+const determineDisplayMode = (dispatch, card) => {
+  switch(card.displayMode) {
+    case 'question':
+      return questionMode(dispatch, card);
+    default:
+      return questionMode(dispatch, card);
+  }
+}
+
+const displayCards = (dispatch, cards) => {
+  return R.pipe(
+    R.map(
+      R.partial(determineDisplayMode, [dispatch]),
+    ),
+    div
+  )(cards);
+}
 
 function view(dispatch, model) {
   return div({ className: 'mw8 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Flashcard Study'),
+    displayCards(dispatch, model.cards),
     pre(JSON.stringify(model, null, 2)),
   ]);
 }
